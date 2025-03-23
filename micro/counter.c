1268ff9bb878f50ffc51161e3635b4a6f0bac304
Ruan <santosruan2021@gmail.com>
2025-03-23 19:11:46 -0300

    feat: piscar leds para feedback de acoes

--- micro/counter.c
+++ micro/counter.c
@@ -10,27 +10,12 @@
 #define HIGH 1
 #define LOW 0
 
-int setup_pir_sensor(){
-    #ifndef PIR_SENSOR_PIN
-        #define PIR_SENSOR_PIN 8
-    #endif
-
-    gpio_init(PIR_SENSOR_PIN);
-    gpio_set_dir(PIR_SENSOR_PIN, GPIO_IN);
-
-    // Ativa pull-down para evitar ruídos
-    gpio_set_pulls(PIR_SENSOR_PIN, false, true);
-
-    printf("Inicializando sensor PIR...\n");
-    sleep_ms(1000);
-    return PICO_OK;
-}
-
-
 int main() {
     stdio_init_all();
+    hard_assert(setup_led() ==  PICO_OK);
     hard_assert(setup_pir_sensor() ==  PICO_OK);
     hard_assert(setup_wifi() ==  PICO_OK);
+    blink_led(PICO_LED_GREEN_PIN, 1000);
 
     int last_state = LOW;
     
@@ -39,9 +24,10 @@ int main() {
 
           // Apenas detecta a transição LOW -> HIGH
         if (pir_state == HIGH && last_state == LOW) {
+            blink_led(PICO_LED_BLUE_PIN, 500);
+            printf("Alguém se mexeu!\n");
             send_http_post();
             cyw43_arch_poll();
-            printf("Alguém se mexeu!\n");
             last_state = HIGH;
             sleep_ms(2500);
         }
--- micro/utils.h
+++ micro/utils.h
@@ -7,6 +7,11 @@
 #include "lwip/tcp.h"
 #include "cJSON.h"
 
+// Pinos utilizados para os LEDs
+#define PICO_LED_GREEN_PIN 11
+#define PICO_LED_BLUE_PIN 12
+#define PICO_LED_RED_PIN 13
+
 // Configurações de Wi-Fi (alterar para o SSID e senha da sua rede)
 #define WIFI_SSID "DEUS E FIEL"
 #define WIFI_PASSWORD "89banana"
@@ -18,8 +23,6 @@
 // Configurações do dashboard
 #define BOARD_ID "1"
 #define LOCATION "Bedroom"
-
-#define PICO_DEFAULT_LED_PIN 11
 
 // Configurações do TCP/IP
 char request_buffer[512];
@@ -71,48 +74,78 @@ void send_http_post()
     tcp_connect(client_pcb, &server_addr, SERVER_PORT, http_connected_callback);
 }
 
+int setup_pir_sensor(){
+    #ifndef PIR_SENSOR_PIN
+        #define PIR_SENSOR_PIN 8
+    #endif
+
+    gpio_init(PIR_SENSOR_PIN);
+    gpio_set_dir(PIR_SENSOR_PIN, GPIO_IN);
+
+    // Ativa pull-down para evitar ruídos
+    gpio_set_pulls(PIR_SENSOR_PIN, false, true);
+
+    printf("Inicializando sensor PIR...\n");
+    sleep_ms(1000);
+    return PICO_OK;
+}
+
+
+
+int setup_led(){
+    
+    
+    gpio_init(PICO_LED_GREEN_PIN);
+    gpio_init(PICO_LED_BLUE_PIN);
+    gpio_init(PICO_LED_RED_PIN);
+    gpio_set_dir(PICO_LED_GREEN_PIN, GPIO_OUT);
+    gpio_set_dir(PICO_LED_BLUE_PIN, GPIO_OUT);
+    gpio_set_dir(PICO_LED_RED_PIN, GPIO_OUT);
+    
+    printf("Inicializando LED...\n");
+    sleep_ms(1000);
+    return PICO_OK;
+}
+
+/**
+ * @brief Acende e apaga o LED vermelho da placa em um intervalo de tempo específico.
+ * @param duration O tempo (em milissegundos) em que o LED ficará aceso. Se o valor
+ *                 for `-1`, será utilizado o intervalo padrão de 500 ms.
+ * @note Esta função assume que o pino do LED foi previamente configurado
+ */
+void blink_led(int color, int duration){
+    #ifndef PICO_DEFAULT_LED_BLINK_INTERVAL
+    #define PICO_DEFAULT_LED_BLINK_INTERVAL 500
+    #endif
+    
+    gpio_put(color, 1);
+    sleep_ms(duration != -1 ? duration : PICO_DEFAULT_LED_BLINK_INTERVAL);
+    gpio_put(color, 0);
+}
 
 int setup_wifi()
 {
-    // // Inicializa a interface de entrada/saída padrão
-    // // stdio_init_all();
-
-    // // Configura o pino do LED como saída
-    // setup_led(PICO_DEFAULT_LED_PIN);
-
-    // Inicializa o Wi-Fi
     if (cyw43_arch_init())
     {
-        printf("Wi-Fi init failed\n");
+        printf("Falha na inicialização do Wi-Fi\n");
         return -1;
     }
 
-    // Habilita o modo Station (STA) do Wi-Fi
     cyw43_arch_enable_sta_mode();
 
-    printf("Connecting to Wi-Fi...\n");
+    printf("Conectando ao Wi-Fi...\n");
 
     // Tenta conectar ao Wi-Fi com o SSID e senha fornecidos
     if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000))
     {
-        printf("Failed to connect.\n");
+        printf("Falha durante a conexão.\n");
         return 1;
     }
-    printf("Connected.\n");
+    printf("Conexão Wi-Fi bem sucedida.\n");
     sleep_ms(1000);
 
     // Obtém o endereço IP do dispositivo
     uint8_t *ip_address = (uint8_t *)&(cyw43_state.netif[0].ip_addr.addr);
-    printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
+    printf("Endereço IP %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
     return PICO_OK;
 }
-
-// int main()
-// {
-//     setup();
-//     while (true)
-//     {
-//         cyw43_arch_poll(); // Processa eventos de rede
-//         sleep_ms(500);
-//     }
-// }
